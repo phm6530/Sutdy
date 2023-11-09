49d6cd75a6ad09ba760a0e9be61acd164c067a7c
hm <phm6530@gmail.com>
2023-11-09 16:01:08 +0900

    23/11/09 리액트정리

--- hook/src/hooks/apiCustum.jsx
+++ hook/src/hooks/apiCustum.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {useCus} from './useCus';
+import {useCus} from './apiCustumCustum';
 import './custum.css';
 
 const BaseUrl = 'https://jsonplaceholder.typicode.com';
 export default function App(){
-    const [ApiValue , fetchUrl] = useCus( BaseUrl , 'users' );
+    const  {data , fetchUrl} = useCus( BaseUrl , 'users' );
     
     return(
             <>  
             <button onClick={() => fetchUrl('users')}> user Api 버튼</button>
             <button onClick={() => fetchUrl('Posts')}> user Post 버튼</button>
             <button onClick={() => fetchUrl('todos')}> user todos  버튼</button>
-            <pre style={{color:'#fff'}}>
-            {ApiValue ? JSON.stringify(ApiValue , null ,2) : '데이터가 없습니다.'}
-                </pre>
+            <pre style={{color:'#fff'}}>{JSON.stringify(data , null ,2)}</pre>
+            {/* {<pre style={{color:'#fff'}}>{JSON.stringify(postData[0] , null ,2)}</pre>} */}
                 
             </>
     )
--- hook/src/hooks/apiCustumCustum.js
+++ hook/src/hooks/apiCustumCustum.js
@@ -1,17 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 
 export function useCus(Url, initalType){
-  const [Value , setValue ] = useState(null);
-  const test = useRef(null)
+  const [data , setValue ] = useState(null);
+  const prevType = useRef(null)
   const fetchUrl = async (type) => {
     fetch(Url + '/' + type)
       .then((res) => res.json())
       .then((data) => {
-        console.log('타입 : ', type);
-        test.current = Value; // 이전 값을 기록
-        console.log('이전데이터 : ',test.current); // 업데이트된 값을 출력
+        console.log('현재 타입 : ', type);
+        console.log('이전 타입 : ',prevType.current); // 업데이트된 값을 출력
+        prevType.current = type; // 이전 값을 기록
         setValue(data); // 현재 값을 업데이트
-        console.log(data);
+        // console.log(data);
         
       });
   }
@@ -19,6 +19,6 @@ export function useCus(Url, initalType){
     fetchUrl(initalType)
   },[]);
 
-  return [ Value , fetchUrl ] ;
+  return { data , fetchUrl } ;
 
 }
