22eac112ca66648a2d497c9b5c2d4c99c6a7e65f
hm <phm6530@gmail.com>
2023-11-09 16:01:39 +0900

    23/11/09 react-todoList

--- todo/src/components/Container.jsx
+++ todo/src/components/Container.jsx
@@ -1,26 +1,37 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState,  useEffect } from "react";
 import { MSG, initData , listArr} from './constants';
 import { handler } from './handler';
-import {useCustumHook} from './cshook';
+// import {useCustumHook} from './cshook';/
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 // import { faLightbulb} from "@fortawesome/free-regular-svg-icons";
 import styled from './css/Container.module.css';
 
+function useCustumHook(key, initial){
+  const [Value ,setValue] = useState(()=>{
+    const LocalData = localStorage.getItem(key); 
+    return LocalData ? JSON.parse(LocalData) : initial ; 
+  });
+  useEffect(()=>{
+    localStorage.setItem(key , JSON.stringify(Value));
+    console.log(Value);
+  },[Value , key]) //
+  return [Value , setValue];
+}
 
 export default function Container() {
   // const [workData, setWorkData] = useState(data.map((e) => ({...e , checked : false })) );
   const [workData, setWorkData] = useCustumHook('TodoList', initData)
   const [mode , Setmode] = useCustumHook('mode' , false);
   // const [value, setValue] = useState(""); 애니메이션 문제로 DOM으로 처리해버림
-  const idx = useRef(0);
   const [active , setActive] = useState(0);
   const [ani , aniSet] = useState(false);
 
+
   const AddWork = (val) => {
     setWorkData((e) => [...e, { 
-      id: idx.current++, 
+      id: workData.length + 1, 
       title: val, 
       checked : false 
     }]);
@@ -34,6 +45,7 @@ export default function Container() {
   };
 
   useEffect(()=>{
+    console.log('로컬 : ',localStorage.getItem('List'))
     aniSet(true);
   },[active]);
 
@@ -56,7 +68,7 @@ export default function Container() {
             {showData.map(item => (
               <li className={`${styled.tab}`} key={item.id}>
                 <label onClick={() => {chk(item.id); aniSet(false) }} className={`chk ${item.checked ? styled.end : ''}`}>
-                  <span className={styled.index}>{item.id + 1}.</span>
+                  <span className={styled.index}>{item.id}.</span>
                   <span className={styled.title}>{item.title}</span>
                   <span className={`${styled.state} ${item.checked === true ? styled.after : styled.before }`}>
                     {item.checked ? 'Completed' : 'NotYet'}
